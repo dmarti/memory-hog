5055967d4e9bb6eb668e28609476d16fe2baf796
Don Marti <dmarti@zgp.org>
2014-04-30 15:53:04 -0700

    Allocate memory until crash.
    Can the shrinker save this program?
    
    Signed-off-by: Don Marti <dmarti@cloudius-systems.com>

--- memory-hog.c
+++ memory-hog.c
@@ -1,5 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define SIZE 31337 * 4096
+
+struct item {
+    char stuff[SIZE];
+    struct item * prev;
+    struct item * next;
+};
+
+struct item * head;
+struct item * tail;
+
+void hog_memory() {
+    struct item * thing;
+    thing = malloc(sizeof(struct item));
+    if (tail) {
+        tail->next = thing;
+        tail = thing;
+    } else {
+        head = thing;
+	tail = thing;
+    }
+    printf("Oink!\n");
+    sleep(1);
+}
+
+/* TODO: Callback function here.  Should free some
+ * items. */
 
 int main (int argc, int **argv) {
-    printf("I'm a memory hog! Oink!\n");
+    head = NULL;
+    tail = NULL;
+    printf("I'm a memory hog!\n");
+    /* TODO: Register a callback here? */
+    for(;;) {
+        hog_memory();
+    }
 }
